a111a62523b5dc6b3b7dc5036cb188cf8b814a34
Henk Boom <henk@henk.ca>
2011-11-10 00:47:52 -0500

    added optional transform constructor arguments

--- transform.lua
+++ transform.lua
@@ -6,9 +6,9 @@ local quaternion = require 'dokidoki.quaternion'
 local transform = pl.class()
 transform._name = 'transform'
 
-function transform:_init()
-  self.pos = vect.zero
-  self.orientation = quaternion.identity
+function transform:_init(pos, orientation)
+  self.pos = pos or vect.zero
+  self.orientation = orientation or quaternion.identity
 end
 
 return transform
