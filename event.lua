33b98863538647537980694c5e18206b1accf6d4
Henk Boom <henk@henk.ca>
2011-11-10 00:45:52 -0500

    fixed potential problem when removing handlers from running events

--- event.lua
+++ event.lua
@@ -10,9 +10,12 @@ function event:_init()
   self._length = 0 -- length of sparse array
   self._indices = {} -- handler->index
   self._handlers = {} -- list of handlers
+  self._running = false
 end
 
 function event:__call(...)
+  self._running = true
+
   local handlers = self._handlers
   for i = 1, self._length do
     local f = handlers[i]
@@ -20,6 +23,11 @@ function event:__call(...)
       f(...)
     end
   end
+
+  self._running = false
+
+  -- we never trigger filters while in the event call loop, so try one now
+  self:_filter_if_appropriate()
 end
 
 function event:add_handler(f)
@@ -30,6 +38,20 @@ function event:add_handler(f)
 
   self._handlers[self._length] = f
   self._indices[f] = self._length
+end
+
+function event:remove_handler(f)
+  local handlers = self._handlers
+  local indices = self._indices
+
+  assert(f ~= nil)
+  assert(indices[f] ~= nil, 'removing handler that isn\'t on this event')
+
+  handlers[indices[f]] = nil
+  indices[f] = nil
+  self._count = self._count - 1
+
+  self:_filter_if_appropriate()
 end
 
 local function filter_out_nils_in_place(t, length)
@@ -59,18 +81,11 @@ local function filter_out_nils_in_place(t, length)
   end
 end
 
-function event:remove_handler(f)
-  local handlers = self._handlers
-  local indices = self._indices
+function event:_filter_if_appropriate()
+  if not self._running and  self._count < self._length/2 then
+    local handlers = self._handlers
+    local indices = self._indices
 
-  assert(f ~= nil)
-  assert(indices[f] ~= nil, 'removing handler that isn\'t on this event')
-
-  handlers[indices[f]] = nil
-  indices[f] = nil
-  self._count = self._count - 1
-
-  if self._count < self._length/2 then
     filter_out_nils_in_place(handlers, self._length)
     self._length = self._count
 
